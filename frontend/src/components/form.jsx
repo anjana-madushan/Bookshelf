d8c06ddf93bec5cf48cf63c0c97448f7afbe646b
AnjanaPasinduMadushan <pbapmadushan@gmail.com>
2023-07-18 20:43:47 +0530

    trying somethings

--- frontend/src/components/form.jsx
+++ frontend/src/components/form.jsx
@@ -6,12 +6,13 @@ const Form = ({book, handleChange, handleSubmit}) => {
   return (
     <div>
       <h1>Add a New Book</h1>
-      <form>
+      <form action="/store-image" method="POST" enctype="multipart/form-data">
       <input type='text' value={book.title}  placeholder='Type the Title' name='title' onChange={handleChange} required/>
       <br/>
       <input type='text' value={book.description} placeholder='Type the Author name' name='description' onChange={handleChange} required/>
       <br/>
-      <input type='text' value={book.coverPic} placeholder='Enter the link of pic' name='coverPic' onChange={handleChange} required/>
+      {/* <input type='text' value={book.coverPic} placeholder='Enter the link of pic' name='coverPic' onChange={handleChange} required/> */}
+      <input type="file"  name="image" multiple></input>
       <br/>
       <button onClick={handleSubmit}>Submit</button>
       </form>
